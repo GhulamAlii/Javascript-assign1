3f048ef5b5397be8b1d00a2b37df330570a5f694
GhulamAlii <sairgust@gmail.com>
2020-06-16 16:16:58 +0500

    update

---
+++ chapter6-9/app.js
@@ -0,0 +1,15 @@
+// 1. Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:  //
+
+var a = 10;
+
+document.write('<h1>Result</h1><br>');
+document.write('<p>The value of a is: '+a+'</p>');
+document.write('<p>-----------------------------------</p><br>');
+document.write('<p>the value of ++a is: '+(++a)+'</p>');
+document.write('<p>Now the value of a is: '+(a)+'</p><br>');
+document.write('<p>the value of a++ is: '+(a++)+'</p>');
+document.write('<p>Now the value of a is: '+(a)+'</p><br>');
+document.write('<p>the value of --a is: '+(--a)+'</p>');
+document.write('<p>Now the value of a is: '+(a)+'</p><br>');
+document.write('<p>Now the value of a-- is: '+(a--)+'</p>');
+document.write('<p>Now the value of a is: '+(a)+'</p><br>');
